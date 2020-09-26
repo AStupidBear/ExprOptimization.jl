098c2945f586467f09f3f65cc01b83e9df549a20
Yao Lu <luyaocns@gmail.com>
2020-09-26 18:25:29 +0800

    mutations

--- src/GeneticProgram/GeneticPrograms.jl
+++ src/GeneticProgram/GeneticPrograms.jl
@@ -10,7 +10,7 @@ import ExprOptimization: optimize
 
 export GeneticProgram
 
-const OPERATORS = [:reproduction, :crossover, :mutation]
+const OPERATORS = [:reproduction, :crossover, :subtree_mutation, :hoist_mutation, :point_mutation]
 
 abstract type InitializationMethod end 
 abstract type SelectionMethod end
@@ -36,6 +36,7 @@ struct GeneticProgram <: ExprOptAlgorithm
     iterations::Int
     max_depth::Int
     parsimony_coefficient::Float64
+    p_point_mutation::Float64
     p_operators::Weights
     init_method::InitializationMethod
     select_method::SelectionMethod
@@ -48,13 +49,16 @@ struct GeneticProgram <: ExprOptAlgorithm
         parsimony_coefficient::Float64,         #parsimony coefficient
         p_reproduction::Float64,                #probability of reproduction operator 
         p_crossover::Float64,                   #probability of crossover operator
-        p_mutation::Float64;                    #probability of mutation operator 
+        p_subtree_mutation::Float64,            #probability of subtree mutation operator 
+        p_hoist_mutation::Float64,              #probability of hoist mutation operator 
+        p_point_mutation::Float64,              #probability of point mutation operator 
+        p_point_replace::Float64;               #probability of point replace 
         init_method::InitializationMethod=RandomInit(),      #initialization method 
         select_method::SelectionMethod=TournamentSelection(),   #selection method 
         track_method::TrackingMethod=NoTracking())   #tracking method 
 
-        p_operators = Weights([p_reproduction, p_crossover, p_mutation])
-        new(pop_size, iterations, max_depth, parsimony_coefficient, p_operators, init_method, select_method, track_method)
+        p_operators = Weights([p_reproduction, p_crossover, p_subtree_mutation, p_hoist_mutation])
+        new(pop_size, iterations, max_depth, parsimony_coefficient, p_point_mutation, p_operators, init_method, select_method, track_method)
     end
 end
 
@@ -141,8 +145,12 @@ function genetic_program(p::GeneticProgram, grammar::Grammar, typ::Symbol, loss:
 
     best_tree, best_loss = evaluate!(p, loss, grammar, pop0, losses0, pop0[1], Inf; verbose=verbose)
     for iter = 1:p.iterations
-        best_expr = get_executable(best_tree, grammar)
-        @master verbose && println("iterations: $iter of $(p.iterations), best_loss: $best_loss, best_expr: $best_expr")
+        @master if verbose 
+            best_expr = get_executable(best_tree, grammar)
+            best_val_loss = loss(best_tree, grammar, validation = true)
+            print("iterations: $iter of $(p.iterations), best_loss: $best_loss")
+            println("best_val_loss: $best_val_loss, best_expr: $best_expr")
+        end
         fill!(losses1, missing)
         i = 0
         while i < p.pop_size
@@ -156,9 +164,17 @@ function genetic_program(p::GeneticProgram, grammar::Grammar, typ::Symbol, loss:
                 ind2,_ = select(p.select_method, pop0, losses0)
                 child = crossover(ind1, ind2, grammar, p.max_depth)
                 pop1[i+=1] = child
-            elseif op == :mutation
+            elseif op == :subtree_mutation
                 ind1,_ = select(p.select_method, pop0, losses0)
-                child1 = mutation(ind1, grammar, dmap, p.max_depth)
+                child1 = subtree_mutation(ind1, grammar, dmap, p.max_depth)
+                pop1[i+=1] = child1
+            elseif op == :hoist_mutation
+                ind1,_ = select(p.select_method, pop0, losses0)
+                child1 = hoist_mutation(ind1, grammar, dmap, p.max_depth)
+                pop1[i+=1] = child1                
+            elseif op == :point_mutation
+                ind1,_ = select(p.select_method, pop0, losses0)
+                child1 = point_mutation(ind1, grammar, dmap, p.max_depth, p.point_replace)
                 pop1[i+=1] = child1
             end
         end
@@ -196,7 +212,7 @@ Random population initialization.
 """
 function initialize(::RandomInit, pop_size::Int, grammar::Grammar, typ::Symbol, 
     dmap::AbstractVector{Int}, max_depth::Int)
-    [rand(RuleNode, grammar, typ, dmap, max_depth) for i = 1:pop_size]
+    [randtree(grammar, typ, dmap, rand(1:max_depth)) for i = 1:pop_size]
 end
 
 """
@@ -284,7 +300,7 @@ end
 
 Crossover genetic operator.  Pick a random node from 'a', then pick a random node from 'b' that has the same type, then replace the subtree 
 """
-function crossover(a::RuleNode, b::RuleNode, grammar::Grammar, max_depth::Int=typemax(Int))
+function crossover(a::RuleNode, b::RuleNode, grammar::Grammar, max_depth::Int)
     child = deepcopy(a)
     crosspoint = sample(b)
     typ = return_type(grammar, crosspoint.ind)
@@ -298,11 +314,11 @@ function crossover(a::RuleNode, b::RuleNode, grammar::Grammar, max_depth::Int=ty
 end
 
 """
-    mutation(a::RuleNode, grammar::Grammar, dmap::AbstractVector{Int}, max_depth::Int=5)
+subtree_mutation(a::RuleNode, grammar::Grammar, dmap::AbstractVector{Int}, max_depth::Int=5)
 
-Mutation genetic operator.  Pick a random node from 'a', then replace the subtree with a random one.
+Subtree Mutation genetic operator.  Pick a random node from 'a', then replace the subtree with a random one.
 """
-function mutation(a::RuleNode, grammar::Grammar, dmap::AbstractVector{Int}, max_depth::Int=5)
+function subtree_mutation(a::RuleNode, grammar::Grammar, dmap::AbstractVector{Int}, max_depth::Int)
     child = deepcopy(a)
     loc = sample(NodeLoc, child)
     mutatepoint = get(child, loc) 
@@ -310,10 +326,79 @@ function mutation(a::RuleNode, grammar::Grammar, dmap::AbstractVector{Int}, max_
     d_node = node_depth(child, mutatepoint)
     d_max = max_depth + 1 - d_node
     if d_max > 0
-        subtree = rand(RuleNode, grammar, typ, dmap, d_max)
+        subtree = randtree(grammar, typ, dmap, rand(1:d_max))
         insert!(child, loc, subtree)
     end
     child
+end
+
+"""
+hoist_mutation(a::RuleNode, grammar::Grammar, dmap::AbstractVector{Int}, max_depth::Int=5)
+
+Hoist Mutation genetic operator. Take the winner of a tournament and select a random subtree from it.
+A random subtree of that subtree is then selected and this is hoisted into the original subtrees location 
+to form an offspring in the next generation. 
+"""
+function hoist_mutation(a::RuleNode, grammar::Grammar, dmap::AbstractVector{Int}, max_depth::Int)
+    child = deepcopy(a)
+    loc = sample(NodeLoc, child)
+    subtree = get(child, loc)
+    subloc = sample(NodeLoc, subtree)
+    subsubtree = get(subtree, subloc)
+    insert!(child, loc, subsubtree)
+    child
+end
+
+"""
+point_mutation(a::RuleNode, grammar::Grammar, dmap::AbstractVector{Int}, max_depth::Int=5)
+
+Point Mutation genetic operator. Point Mutation takes the winner of a tournament and selects 
+random nodes from it to be replaced. Terminals are replaced by other terminals and functions 
+are replaced by other functions that require the same number of arguments as the original node. 
+The resulting tree forms an offspring in the next generation.
+"""
+point_mutation(a::RuleNode, grammar::Grammar, dmap::AbstractVector{Int}, max_depth::Int, point_replace::Float64) = 
+    point_mutation!(deepcopy(a), grammar,dmap, max_depth, point_mutation)
+function point_mutation!(a, grammar, dmap, max_depth, point_replace)
+    if rand() < point_replace
+        a._val = iseval(grammar, a) ? Core.eval(a, grammar) : a._val
+        a.ind = filter(eachindex(grammar.rules)) do r
+            return_type(grammar, r) == return_type(grammar, a) &&
+            child_types(garmmar, r) == child_types(grammar, a)
+        end |> rand
+    end
+    for c in a.children
+        point_mutation!(c, grammar, dmap, max_depth, point_mutation)
+    end
+    return a
+end
+
+function randtree(grammar, typ, dmap, d_max)
+    if rand() < 0.5
+        rand(RuleNode, grammar, typ, dmap, d_max)
+    else
+        rand_full(grammar, typ, dmap, d_max)
+    end
+end
+
+function rand_full(grammar, typ, dmap, max_depth, bin=nothing)
+    rules = grammar[typ]
+    if max_depth > 1
+        rule_index = StatsBase.sample(filter(r->!isterminal(grammar, r) && dmap[r] ≤ max_depth, rules))
+    else
+        rule_index = StatsBase.sample(filter(r->dmap[r] ≤ max_depth, rules))
+    end
+
+    rulenode = iseval(grammar, rule_index) ?
+        RuleNode(bin, rule_index, Core.eval(grammar, rule_index)) :
+        RuleNode(bin, rule_index)
+
+    if !grammar.isterminal[rule_index]
+        for ch in child_types(grammar, rule_index)
+            push!(rulenode.children, rand_full(grammar, ch, dmap, max_depth-1, bin))
+        end
+    end
+    return rulenode
 end
 
 end #module
